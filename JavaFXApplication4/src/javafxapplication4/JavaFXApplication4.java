aeae917f43cce58e8845d072056bc245b2433ef0
rafaelsantos-dev <leafar174@gmail.com>
2022-02-14 15:44:36 -0300

    update setTitle
    Testando integração com VS Code e Netbeans

--- JavaFXApplication4/src/javafxapplication4/JavaFXApplication4.java
+++ JavaFXApplication4/src/javafxapplication4/JavaFXApplication4.java
@@ -21,7 +21,8 @@ public class JavaFXApplication4 extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        stage.setTitle("Calculadora de Substituição Tributária - ATC");
+
         stage.setScene(scene);
         stage.show();
     }
